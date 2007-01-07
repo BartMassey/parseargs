526bf95068ac955fed838e800e9c379b79f15700
Bart Massey <bart@cs.pdx.edu>
2007-01-06 20:59:44 -0800

    setup code for parseargs module
    darcs-hash:20070107045944-1f50a-3c4fc9e2ccbe8fb2d40993a256eb86c79ff68389.gz

---
+++ parseargs.hs
@@ -0,0 +1,90 @@
+--- Full-featured argument parser
+--- Bart Massey 2007/01/06
+---
+--- Copyright (C) 2007 Bart Massey
+--- ALL RIGHTS RESERVED
+
+module ParseArgs (baseName)
+where
+
+
+import Data.List
+import qualified Data.Map as Map
+
+--- The main job of this module is to provide parseArgs.
+--- See below for its contract.
+
+--- The kinds of "value" an argument can have.
+data ArgVal =
+    ArgValFlag |
+    ArgValString String |
+    ArgValInt Int
+
+--- The corresponding argument type requirements
+data ArgAtype =
+    ArgAtypeFlag |
+    ArgAtypeString |
+    ArgAtypeInt
+
+--- The description of an argument, suitable for
+--- messages and for parsing.
+--- It is expected that each argument be identified
+--- by an enumeration value, so that its value can easily
+--- be referenced later.
+data (Ord a) => Arg a =
+    Arg { argIndex :: a,
+          argAtype :: ArgAtype,
+          argAbbr :: Maybe Char,
+          argName :: Maybe String,
+          argExprName :: Maybe String,
+          argDesc :: String }
+
+--- The full description of the parsing problem for
+--- parseArgs to swallow.
+data ArgDesc a =
+    ArgDesc { argDescArgs :: [ Arg a ],
+              argDescPosnArgs :: [ Arg a ],
+              argDescOptArgs :: [ Arg a ],
+              argDescComplete :: Bool }
+
+--- The data structure parseArgs produces.
+data (Ord a) => Args a =
+    Args { args :: Map.Map a ArgVal,
+           argsProgName :: String,
+           argsRest :: [ String ] }
+
+--- Return the filename part of 'path'.
+--- Unnecessarily efficient implementation does a single
+--- tail-call traversal with no construction.
+baseName :: String -> String
+baseName s =
+    let s' = dropWhile (/= '/') s in
+    if null s' then s else baseName (tail s')
+
+
+--- Given an arg desc, produce a description string.
+arg_string :: (Ord a) => Arg a -> String
+arg_string (Arg { argAbbr = abbr,
+                  argName = name,
+                  argExprName = exprName }) =
+    case exprName of
+      Nothing -> flag_part ""
+      Just s -> (flag_part " ") ++ "<" ++ s ++ ">"
+    where
+      flag_part :: String -> String
+      flag_part sep =
+      	  case name of
+            Nothing -> ""
+            Just s ->
+                (case abbr of
+                  Nothing -> ""
+                  Just c -> "-" ++ [c] ++ ",") ++
+                ("--" ++ s) ++
+                sep
+
+--- Given a description of the arguments, parseArgs produces
+--- a map from the arguments to their "values" and some other
+--- useful byproducts.
+
+--- parseArgs :: ArgDesc a -> [ String ] -> Args a
+--- parseArgs argd argv =
